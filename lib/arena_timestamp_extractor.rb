b8cbe7acafcb3489780098329c4a7ab2f6af9844
Dan Finnie and Mark McDonald <pair+dfinnie+mark.mcdonald.ga@gmail.com>
2015-02-06 22:45:02 -0500

    Move knowledge of file naming to VideoFrameExtractor

--- lib/arena_timestamp_extractor.rb
+++ lib/arena_timestamp_extractor.rb
@@ -13,13 +13,11 @@ class ArenaTimestampExtractor
   end
 
   def extract
-    VideoFrameExtractor.new(video_file.path).execute!
     find_obj = FindObj.new("warcraft_logo.png")
-    Dir["/tmp/screenshot*.png"].map do |scene_filename|
-      puts "processing #{scene_filename}" if $DEBUG
-      descriptor_count = find_obj.execute!(scene_filename)
-      timestamp = scene_filename[/\d+/].to_i
-      DescriptorPairing.new(descriptor_count, timestamp)
+     VideoFrameExtractor.new(video_file.path).frames.map do |frame|
+      puts "processing #{frame.filename}" if ENV['TWITCH_DEBUG']
+      descriptor_count = find_obj.execute!(frame.filename)
+      DescriptorPairing.new(descriptor_count, frame.timestamp)
     end.select do |pair|
       pair.descriptor_count > 100
     end.map(&:timestamp).sort
--- lib/video_frame_extractor.rb
+++ lib/video_frame_extractor.rb
@@ -1,8 +1,9 @@
 require 'streamio-ffmpeg'
 
 class VideoFrameExtractor
+  Frame = Struct.new(:filename, :timestamp)
 
-  def initialize(file_path='spec/fixtures/605443292-0.flv', work_directory='/tmp')
+  def initialize(file_path, work_directory='/tmp')
     @file_path = file_path
     @work_directory = work_directory
   end
@@ -11,13 +12,12 @@ class VideoFrameExtractor
     @duration ||= FFMPEG::Movie.new(@file_path).duration
   end
 
-  def extracted_frames
-    Dir.glob("#{@work_directory}/screenshot-*.png")
-  end
-
-  def execute!
+  def frames
     index_digits = Math.log10(duration).to_i + 1
     `ffmpeg -i #{@file_path} -r 1 #{@work_directory}/screenshot-%0#{index_digits}d.png -loglevel error`
+    Dir.glob("#{@work_directory}/screenshot-*.png").map do |filename|
+      Frame.new(filename, filename[/\d+/].to_i)
+    end
   end
 
 end
