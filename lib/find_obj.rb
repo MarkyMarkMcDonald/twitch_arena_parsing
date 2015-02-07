0350c4d3745c4eb65cfe389b01e9886c2dab9f01
Dan Finnie and Mark McDonald <pair+dfinnie+mark.mcdonald.ga@gmail.com>
2015-02-06 23:04:13 -0500

    Privatized and modernized FindObj

--- lib/find_obj.rb
+++ lib/find_obj.rb
@@ -6,6 +6,24 @@ require 'benchmark'
 class FindObj
   include OpenCV
 
+  CV_SURF_PARAMS = CvSURFParams.new(1500)
+
+  def initialize(object_filename)
+    object = IplImage.load(object_filename, CV_LOAD_IMAGE_GRAYSCALE)
+    @object_keypoints, @object_descriptors = object.extract_surf(CV_SURF_PARAMS)
+  end
+
+  def execute!(scene_filename)
+    image = IplImage.load(scene_filename, CV_LOAD_IMAGE_GRAYSCALE)
+
+    image_keypoints, image_descriptors = image.extract_surf(CV_SURF_PARAMS)
+
+    ptpairs = find_pairs(@object_keypoints, @object_descriptors, image_keypoints, image_descriptors)
+    ptpairs.size
+  end
+
+  private
+
   def compare_surf_descriptors(d1, d2, best, length)
     raise ArgumentError unless (length % 4) == 0
     total_cost = 0
@@ -46,64 +64,13 @@ class FindObj
 
   def find_pairs(object_keypoints, object_descriptors,
                  image_keypoints, image_descriptors)
-    ptpairs = []
-    object_descriptors.size.times { |i|
-      kp = object_keypoints[i]
-      descriptor = object_descriptors[i]
+    object_descriptors.zip(object_keypoints).each_with_index.inject([]) do |memo, ((descriptor, kp), i)|
       nearest_neighbor = naive_nearest_neighbor(descriptor, kp.laplacian, image_keypoints, image_descriptors)
       unless nearest_neighbor.nil?
-        ptpairs << i
-        ptpairs << nearest_neighbor
+        memo << i
+        memo << nearest_neighbor
       end
-    }
-    ptpairs
-  end
-
-  def locate_planar_object(object_keypoints, object_descriptors,
-                           image_keypoints, image_descriptors, src_corners)
-    ptpairs = find_pairs(object_keypoints, object_descriptors, image_keypoints, image_descriptors)
-    n = ptpairs.size / 2
-    return nil if n < 4
-
-    pt1 = []
-    pt2 = []
-    n.times { |i|
-      pt1 << object_keypoints[ptpairs[i * 2]].pt
-      pt2 << image_keypoints[ptpairs[i * 2 + 1]].pt
-    }
-
-    _pt1 = CvMat.new(1, n, CV_32F, 2)
-    _pt2 = CvMat.new(1, n, CV_32F, 2)
-    _pt1.set_data(pt1)
-    _pt2.set_data(pt2)
-    h = CvMat.find_homography(_pt1, _pt2, :ransac, 5)
-
-    dst_corners = []
-    4.times { |i|
-      x = src_corners[i].x
-      y = src_corners[i].y
-      z = 1.0 / (h[6][0] * x + h[7][0] * y + h[8][0])
-      x = (h[0][0] * x + h[1][0] * y + h[2][0]) * z
-      y = (h[3][0] * x + h[4][0] * y + h[5][0]) * z
-      dst_corners << CvPoint.new(x.to_i, y.to_i)
-    }
-
-    dst_corners
-  end
-
-  CV_SURF_PARAMS = CvSURFParams.new(1500)
-
-  def initialize(object_filename)
-    object = IplImage.load(object_filename, CV_LOAD_IMAGE_GRAYSCALE)
-    @object_keypoints, @object_descriptors = object.extract_surf(CV_SURF_PARAMS)
-  end
-
-  def execute!(scene_filename)
-    image = IplImage.load(scene_filename, CV_LOAD_IMAGE_GRAYSCALE)
-
-    image_keypoints, image_descriptors = image.extract_surf(CV_SURF_PARAMS)
-
-    ptpairs = find_pairs(@object_keypoints, @object_descriptors, image_keypoints, image_descriptors)
-    ptpairs.size
+      memo
+    end
   end
 end
